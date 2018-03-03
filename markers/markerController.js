8411c85e14015f7a679348c1e1ce1b07e0d25bf3
LAMbl4 <36377115+LAMbl4@users.noreply.github.com>
2018-03-03 15:01:34 +0400

    Add files via upload

--- markers/markerController.js
+++ markers/markerController.js
@@ -25,34 +25,34 @@ router.post('/', function (req, res) {
 				} 
 				else 
 				{					
-					return res.status(500).send(err);
+					return res.status(500).send({ succes: false, message: 'Error!' });
 				}
 			}
-            res.status(200).send(marker);
+            res.status(200).send({ succes: true, message: 'Marker create!' });
         });
 });
 
 // RETURNS ALL THE Markers IN THE DATABASE
 router.get('/', function (req, res) {
     Marker.find({}, function (err, markers) {
-        if (err) return res.status(500).send("There was a problem finding the Markers.");
-        res.status(200).send(markers);
+        if (err) return res.status(500).send({ succes: false, message: 'There was a problem finding the Markers.' });
+        res.status(200).send({ succes: true, message: 'return Markers.', result: markers});
     });
 });
 
 // GETS A SINGLE MARKER FROM THE DATABASE
-router.get('/:id', function (req, res) {
-    Marker.findById(req.params.id, function (err, marker) {
-        if (err) return res.status(500).send("There was a problem finding the marker.");
-        if (!marker) return res.status(404).send("No Marker found.");
-        res.status(200).send(marker);
+router.get('/:fb_id', function (req, res) {
+    Marker.findOne({ 'fb_id': req.params.fb_id }, req.body, function (err, marker) {
+        if (err) return res.status(500).send({ succes: false, message: 'There was a problem finding the marker.' });
+        if (!marker) return res.status(404).send({ succes: false, message: 'No Marker found.' });
+        res.status(200).send({ succes: true, message: 'return Marker.', result: marker });
     });
 });
 
 // DELETES A MARKER FROM THE DATABASE
 router.delete('/:id', function (req, res) {
     Marker.findByIdAndRemove(req.params.id, function (err, marker) {
-        if (err) return res.status(500).send("There was a problem deleting the marker.");
+		if (err) return res.status(500).send({ succes: false, message: 'There was a problem deleting the marker.' });
         res.status(200).send("Marker "+ Marker.fb_id +" was deleted.");
     });
 });
@@ -61,8 +61,8 @@ router.delete('/:id', function (req, res) {
 router.put('/:id', function (req, res) {
 
     Marker.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, marker) {
-        if (err) return res.status(500).send("There was a problem updating the Marker.");
-        res.status(200).send(marker);
+        if (err) return res.status(500).send({ succes: false, message: 'There was a problem updating the Marker.' });
+        res.status(200).send({ succes: true, message: 'marker was update.' });
     });
 });
 
